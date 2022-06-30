8cdcb09544e7dfa87e5742138fd7cdc011863d56
udi.mariano.r <udi.mariano.r@gmail.com>
2022-06-30 20:55:47 -0300

    agrego error

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,23 +1,32 @@
 var botonBuscar = document.querySelector("#buscar-paciente");
-botonBuscar.addEventListener("click", function(){
+botonBuscar.addEventListener("click", function () {
     //console.log("buscando pacientes");
     //para conectarme con la api
     //https://alura-es-cursos.github.io/api-pacientes/pacientes.json
     //uso un objeto XMLHttpRequest (ver)
     var xhr = new XMLHttpRequest;
     xhr.open("GET", "https://alura-es-cursos.github.io/api-pacientes/pacientes.json");
-    xhr.addEventListener("load", function(){
-        var respuesta = xhr.responseText;
-        //console.log(respuesta);
-        //console.log(typeof(respuesta));
+    xhr.addEventListener("load", function () {
 
-        var pacientes = JSON.parse(respuesta);
-        //console.log(pacientes);
-        //console.log(typeof(pacientes));
+        var errorAjax = document.querySelector("#error-ajax");
 
-        pacientes.forEach(function(paciente){
-            adicionarPaciente(paciente);
-        });
+        if (xhr.status == 200) {
+            errorAjax.classList.add("invisible");
+            var respuesta = xhr.responseText;
+            //console.log(respuesta);
+            //console.log(typeof(respuesta));
+
+            var pacientes = JSON.parse(respuesta);
+            //console.log(pacientes);
+            //console.log(typeof(pacientes));
+
+            pacientes.forEach(function (paciente) {
+                adicionarPaciente(paciente);
+            });
+        }else{
+            errorAjax.classList.remove("invisible");
+        }
+
     });
     xhr.send();
 });
